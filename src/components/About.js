9c7f13923c04623fc0fb27edd3d840a0e9f0e840
Paul Shumate <pmshuma@aol.com>
2024-03-06 10:52:23 -0600

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { image } from "../data/data";
+// src/components/About.js
+import React from 'react';
+import { image } from '../data/data';
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Some content about me.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
+
